988b0ecfc892c2ec12248b393420b8b50421a29a
MiltonCarb <carb.milton@gmail.com>
2016-09-22 13:35:10 -0300

    TestMain added

--- src/main/java/ar/com/app/TestMain.java
+++ src/main/java/ar/com/app/TestMain.java
@@ -7,72 +7,47 @@ import com.ecwid.maleorang.MailchimpClient;
 import com.ecwid.maleorang.MailchimpException;
 import com.ecwid.maleorang.MailchimpObject;
 
-import ar.com.bit8.entities.Bit8Response;
 import ar.com.mailchimp.method.v3_0.campaign.CreateCampaignMethod;
 import ar.com.mailchimp.method.v3_0.campaign.CreateCampaignResponse;
 import ar.com.mailchimp.method.v3_0.campaign.SendCampaignMethod;
 import ar.com.mailchimp.method.v3_0.campaign.SetCampaignContentMethod;
-import ar.com.mailchimp.method.v3_0.lists.GetListsMethod;
-import ar.com.mailchimp.method.v3_0.lists.GetListsResponse;
-import ar.com.mailchimp.method.v3_0.lists.ListInfo;
+import ar.com.mailchimp.method.v3_0.members.AddMemberMethod;
 import ar.com.mailchimp.method.v3_0.templates.GetTemplatesMethod;
 import ar.com.mailchimp.method.v3_0.templates.GetTemplatesResponse;
 import ar.com.mailchimp.method.v3_0.templates.TemplateInfo;
 
-public class Main {
+public class TestMain {
 
-	private static String apiKey = "API-KEY HERE";
+	private static String apiKey = "INSERT APIKEY HERE";
 	private static MailchimpClient client = new MailchimpClient(apiKey);
 	
 	
 	public static void main(String[] args) throws IOException, MailchimpException {
 		
 		String campaign_id;
-		String game_id = "123";
-		String templateName = "";
-		String listName = "";
+
+		addSubscriber();
 		
-		
-		campaign_id = createCampaign(game_id);
-		
-		Bit8Response bit8Response = getBit8Result(game_id);
-		setCampaignContent(bit8Response, templateName, listName, campaign_id);
+		campaign_id = createCampaign();
+		setCampaignContent(campaign_id);
 		sendCampaign(campaign_id);
 		
-		//sendCampaigns(bit8);
 	}
 
-	public static Bit8Response getBit8Result(String game_id){		
-		
-		Bit8Response bit8Response = new Bit8Response();
-		
-		//Call to Bit8 to retrieve the results of the lottery game
-		String draw_number = "#1489";
-		String game_name = "Lotto Lottery";
-		String result_numbers = "04-11-14-20-27-32";
-		//End of the Bit8 call
-		
-		bit8Response.setDraw_number(draw_number);
-		bit8Response.setGame_name(game_name);
-		bit8Response.setResult_numbers(result_numbers);
-		
-		return bit8Response;
-		
-	}
 	
-	public static String createCampaign(String game_id) throws IOException, MailchimpException{
-
+	public static String createCampaign() throws IOException, MailchimpException{
+		
 		CreateCampaignMethod createCampaign = new CreateCampaignMethod();
 		createCampaign.setType("regular");
 		
 		MailchimpObject settings = new MailchimpObject();
-		settings.mapping.put("subject_line", "Lotto Lottery Results");
-		settings.mapping.put("from_name", "LottoMaster");
-		settings.mapping.put("reply_to", "somemail@example.com");
-		settings.mapping.put("title", "Campaign game: " + game_id);
+		settings.mapping.put("subject_line", "Test");
+		settings.mapping.put("from_name", "Testomator");
+		settings.mapping.put("reply_to", "INSERT VALID E-MAIL ADRESS HERE");
+		settings.mapping.put("title", "Test Campaign");
 		
 		MailchimpObject recipients = new MailchimpObject();
-		recipients.mapping.put("list_id", getListIdByName("List+"+game_id) );
+		recipients.mapping.put("list_id", "INSERT LIST ID HERE" );
 		
 		createCampaign.mapping.put("recipients", recipients);
 		createCampaign.mapping.put("settings",settings);
@@ -83,54 +58,50 @@ public class Main {
 	}
 	
 	
-	public static void setCampaignContent(Bit8Response bit8Response, String templateName, String listName, String campaignId) throws IOException, MailchimpException{
-		
+	public static void setCampaignContent(String campaignId) throws IOException, MailchimpException{
+			
 		SetCampaignContentMethod setCampaignContentMethod = new SetCampaignContentMethod();
 		
 		MailchimpObject template = new MailchimpObject();
-		template.mapping.put("id", getTemplateIdByName(templateName));
+		template.mapping.put("id", getTemplateIdByName("name_of_the_template"));
 		
 		MailchimpObject sections = new MailchimpObject();
-		sections.mapping.put("draw", "#1489");
-		sections.mapping.put("game", "Lotto Lottery ");
-		sections.mapping.put("result", "04-11-14-20-27-32");
+		sections.mapping.put("name", "Tester");
+		sections.mapping.put("lastname", "Testerin");
 		template.mapping.put("sections", sections);
 		
 		setCampaignContentMethod.setTemplate(template);
 		setCampaignContentMethod.setCampaign_id(campaignId);
 		
 		client.execute(setCampaignContentMethod);
-		System.out.println("*********************");
-		System.out.println(campaignId);
-		System.out.println("*********************");
-		
+	
 	}
 	
 	
 	public static void sendCampaign(String campaignId) throws IOException, MailchimpException{
-			
+		
 		SendCampaignMethod sendMethod= new SendCampaignMethod();
 		sendMethod.setCampaign_id(campaignId);
 		client.execute(sendMethod);
 			
 	}
-
-
-	public static String getListIdByName(String list_name) throws IOException, MailchimpException{
+	
+	
+	public static void addSubscriber() throws IOException, MailchimpException{
 		
-		GetListsMethod getLists = new GetListsMethod();
-		GetListsResponse execute = client.execute(getLists);
-		List<ListInfo> lists = execute.getLists();
+		AddMemberMethod addMemberMethod = new AddMemberMethod();
 		
-		for (ListInfo listInfo : lists) {
-			if(listInfo.getName().equals(list_name))
-				return listInfo.getId();
-		}
-		
-		return null;
+		addMemberMethod.setList_id("INSERT LIST ID HERE");
+		addMemberMethod.setEmail_address("INSERT E-MAIL TO SUBSCRIBE HERE");
+		addMemberMethod.setStatus("subscribed");
+		MailchimpObject merge_fields = new MailchimpObject();
+		merge_fields.mapping.put("USERNAME", "tester1991");
+		addMemberMethod.setMerge_fields(merge_fields);
+		System.out.println(addMemberMethod.toJson());
+		System.out.println(client.execute(addMemberMethod));
 		
 	}
-	
+
 	
 	public static Integer getTemplateIdByName(String templateName) throws IOException, MailchimpException{
 
